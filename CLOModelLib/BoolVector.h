1182b530a0bd9ba6c8f1db8f719a73fb645a5cdc
Luca Beldi <luca.beldi@twentyfouram.com>
2015-06-18 17:38:02 +0100

    moved BoolVector

---
+++ CLOModelLib/BoolVector.h
@@ -0,0 +1,33 @@
+#ifndef BoolVector_h__
+#define BoolVector_h__
+#include <QList>
+#include <QMetaType>
+#include "AbstractBbgVect.h"
+class BoolVectorPrivate;
+class BoolVector : public AbstractBbgVect
+{
+    Q_DECLARE_PRIVATE(BoolVector)
+protected:
+	virtual void UnpackVector();
+	virtual bool IsValid() const override;
+	virtual QDataStream& LoadOldVersion(QDataStream& stream) override;
+public:
+	virtual QRegExpValidator* GetValidator(QObject* parent = 0) const override;
+    BoolVector();
+	BoolVector(const BoolVector& Vec);
+	BoolVector(const QString& Vec);
+	BoolVector(const QString& Vec,const QDate& Anchor);
+	bool GetValue(const QDate& index) const;
+	bool GetValue(int index) const;
+	int NumElements() const;
+	BoolVector& operator=(const QString& a);
+	friend QDataStream& operator<<(QDataStream & stream, const BoolVector& flows);
+	friend QDataStream& operator>>(QDataStream & stream, BoolVector& flows);
+};
+QDataStream& operator<<(QDataStream & stream, const BoolVector& flows);
+QDataStream& operator>>(QDataStream & stream, BoolVector& flows);
+Q_DECLARE_METATYPE(BoolVector)
+
+#endif // BoolVector_h__
+
+
---
+++ CLOModelLib/BoolVector_p.h
@@ -0,0 +1,19 @@
+#ifndef BoolVector_p_h__
+#define BoolVector_p_h__
+#include "AbstractBbgVect_p.h"
+#include "BoolVector.h"
+class BoolVectorPrivate : public AbstractBbgVectPrivate
+{
+    Q_DECLARE_PUBLIC(BoolVector)
+private:
+    BoolVectorPrivate(const BoolVectorPrivate& other);
+public:
+    BoolVectorPrivate(BoolVector* q);
+    BoolVectorPrivate(BoolVector* q, const BoolVectorPrivate& other);
+    virtual BoolVectorPrivate& operator=(const BoolVectorPrivate& other);
+protected:
+    QList<bool> m_VectVal;
+    friend QDataStream& operator<<(QDataStream & stream, const BoolVector& flows);
+    friend QDataStream& operator>>(QDataStream & stream, BoolVector& flows);
+};
+#endif // BoolVector_p_h__

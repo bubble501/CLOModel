6d016553e4a838e931f640b4361429b3d80a24bd
Luca Beldi <luca.beldi@twentyfouram.com>
2015-06-19 12:39:19 +0100

    Refactored Generic Cash Flow

---
+++ CLOModelLib/GenericCashFlow_p.h
@@ -0,0 +1,23 @@
+#include "GenericCashFlow.h"
+#include "BackwardCompatibilityInterface_p.h"
+#include <QMap>
+#include <QSet>
+class GenericCashFlowPrivate : public BackwardInterfacePrivate
+{
+    Q_DECLARE_PUBLIC(GenericCashFlow)
+private:
+    GenericCashFlowPrivate(const GenericCashFlowPrivate& other);
+public:
+    GenericCashFlowPrivate(GenericCashFlow* q);
+    GenericCashFlowPrivate(GenericCashFlow* q, const GenericCashFlowPrivate& other);
+    virtual GenericCashFlowPrivate& operator=(const GenericCashFlowPrivate& other);
+protected:
+    static bool SamePeriod(const QDate& a, const QDate& b, GenericCashFlow::CashFlowAggregation Freq);
+    QMap<QDate, QHash<qint32, double>*	> m_CashFlows;
+    QHash<qint32, QString> m_CashFlowLabels;
+    QSet<qint32> m_Stocks;
+    GenericCashFlow::CashFlowAggregation m_AggregationLevel;
+    bool m_AdjustHolidays;
+    friend QDataStream& operator<<(QDataStream & stream, const GenericCashFlow& flows);
+    friend QDataStream& operator>>(QDataStream & stream, GenericCashFlow& flows);
+};

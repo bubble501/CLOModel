42e3feae7e76bcb66c32e226802b81dae3533e19
Luca Beldi <luca.beldi@twentyfouram.com>
2015-06-19 10:16:54 +0100

    Refactored Repayment Vector

---
+++ CLOModelLib/RepaymentVector.h
@@ -0,0 +1,41 @@
+#ifndef RepaymentVector_h__
+#define RepaymentVector_h__
+#include <QList>
+#include <QMetaType>
+#include "AbstractBbgVect.h"
+class RepaymentVectorPrivate;
+class CLOMODELLIB_EXPORT RepaymentVector : public AbstractBbgVect
+{
+    Q_DECLARE_PRIVATE(RepaymentVector)
+public:
+	enum RepaymentMethods : qint8 {
+		Invalid=-1
+		, InterestOnly = 0x1
+		, Annuity = 0x2
+		, Linear = 0x4
+		, Capitalization = 0x8
+	};
+protected:
+	virtual QRegExpValidator* GetValidator(QObject* parent = 0) const override;
+	virtual void UnpackVector() override;
+	virtual bool IsValid() const override;
+	virtual QDataStream& LoadOldVersion(QDataStream& stream) override;
+public:
+    RepaymentVector();
+	RepaymentVector(const RepaymentVector& Vec);
+	RepaymentVector(const QString& Vec);
+	RepaymentVector(const QString& Vec, const QDate& Anchor);
+	RepaymentMethods GetValue(const QDate& index) const;
+	RepaymentMethods GetValue(int index) const;
+	QString GetValueString(const QDate& index) const;
+	QString GetValueString(int index) const;
+	int NumElements() const;
+    RepaymentVector& operator=(const RepaymentVector& a);
+	RepaymentVector& operator=(const QString& a);
+	friend QDataStream& operator<<(QDataStream & stream, const RepaymentVector& flows);
+	friend QDataStream& operator>>(QDataStream & stream, RepaymentVector& flows);
+};
+QDataStream& operator<<(QDataStream & stream, const RepaymentVector& flows);
+QDataStream& operator>>(QDataStream & stream, RepaymentVector& flows);
+Q_DECLARE_METATYPE(RepaymentVector)
+#endif // RepaymentVector_h__
---
+++ CLOModelLib/RepaymentVector_p.h
@@ -0,0 +1,24 @@
+#ifndef RepaymentVector_p_h__
+#define RepaymentVector_p_h__
+
+#include "AbstractBbgVect_p.h"
+#include "RepaymentVector.h"
+class RepaymentVectorPrivate : public AbstractBbgVectPrivate
+{
+    Q_DECLARE_PUBLIC(RepaymentVector)
+private:
+    RepaymentVectorPrivate(const RepaymentVectorPrivate& other);
+public:
+    RepaymentVectorPrivate(RepaymentVector* q);
+    RepaymentVectorPrivate(RepaymentVector* q, const RepaymentVectorPrivate& other);
+    virtual RepaymentVectorPrivate& operator=(const RepaymentVectorPrivate& other);
+protected:
+    QList<RepaymentVector::RepaymentMethods> m_VectVal;
+    QString RepaymentMethodsToString(int a) const;
+    QString RepaymentMethodsToString(RepaymentVector::RepaymentMethods a) const;
+    RepaymentVector::RepaymentMethods StringToRepaymentMethods(const QString& a) const;
+    static RepaymentVector::RepaymentMethods ConvertRepayment(QChar a);
+    friend QDataStream& operator<<(QDataStream & stream, const RepaymentVector& flows);
+    friend QDataStream& operator>>(QDataStream & stream, RepaymentVector& flows);
+};
+#endif // RepaymentVector_p_h__

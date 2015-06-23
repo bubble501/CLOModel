29fac785a2801f4a1ac92b25b9bc16392d22cec8
Luca Beldi <luca.beldi@twentyfouram.com>
2015-06-23 10:36:05 +0100

    Refactored DeferredInterestTrigger

---
+++ CLOModelLib/DeferredInterestTrigger.cpp
@@ -0,0 +1,322 @@
+#include "DeferredInterestTrigger.h"
+#include "Private/DeferredInterestTrigger_p.h"
+#include <QDataStream>
+DEFINE_PUBLIC_COMMONS(DeferredInterestTrigger)
+DEFINE_PUBLIC_COMMONS_COPY(DeferredInterestTrigger)
+
+DeferredInterestTriggerPrivate::DeferredInterestTriggerPrivate(DeferredInterestTrigger *q)
+	:AbstractTriggerPrivate(q)
+{}
+
+DeferredInterestTrigger::DeferredInterestTrigger(DeferredInterestTriggerPrivate *d, const DeferredInterestTrigger& other)
+	:AbstractTrigger(d,other)
+{
+    d->m_TargetSeniority = other.d_func()->m_TargetSeniority;
+    d->m_TargetSeniorityLevel = other.d_func()->m_TargetSeniorityLevel;
+    d->m_TargetSize = other.d_func()->m_TargetSize;
+    d->m_TargetCoupon = other.d_func()->m_TargetCoupon;
+    d->m_SenioritySide = other.d_func()->m_SenioritySide;
+    d->m_SizeSide = other.d_func()->m_SizeSide;
+    d->m_CouponSide = other.d_func()->m_CouponSide;
+}
+DeferredInterestTrigger& DeferredInterestTrigger::operator=(const DeferredInterestTrigger& other){
+	Q_D(DeferredInterestTrigger);
+	AbstractTrigger::operator=(other);
+    d->m_TargetSeniority = other.d_func()->m_TargetSeniority;
+    d->m_TargetSeniorityLevel = other.d_func()->m_TargetSeniorityLevel;
+    d->m_TargetSize = other.d_func()->m_TargetSize;
+    d->m_TargetCoupon = other.d_func()->m_TargetCoupon;
+    d->m_SenioritySide = other.d_func()->m_SenioritySide;
+    d->m_SizeSide = other.d_func()->m_SizeSide;
+    d->m_CouponSide = other.d_func()->m_CouponSide;
+	return *this;
+}
+DeferredInterestTrigger::DeferredInterestTrigger(DeferredInterestTriggerPrivate *d)
+    :AbstractTrigger(d, AbstractTrigger::DeferredInterestTrigger)
+{
+    d->m_TargetSeniority = "0";
+    d->m_TargetSeniorityLevel = "1";
+    d->m_TargetSize = "0";
+    d->m_TargetCoupon = "1";
+    d->m_SenioritySide = TriggerSenioritySide::Invalid;
+    d->m_SizeSide = TriggerSizeSide::Invalid;
+    d->m_CouponSide = TriggerCouponSide::Invalid;
+    d->m_TargetSeniorityLevel.SetShift(-1);
+    d->m_TargetCoupon.SetShift(-1);
+}
+
+DeferredInterestTrigger::DeferredInterestTrigger(
+    QString TargetSenior
+    , QString TargetSeniorLevel
+    , QString TargetSize
+    , QString TargetCoupon
+    , TriggerSizeSide szd
+    , TriggerSenioritySide ssd
+    , TriggerCouponSide scd
+    , const QString& lab
+    )
+    : DeferredInterestTrigger(lab)
+{
+    Q_D(DeferredInterestTrigger);
+    d->m_TargetSeniority = TargetSenior;
+    d->m_TargetSeniorityLevel = TargetSeniorLevel;
+    d->m_TargetSize = TargetSize;
+    d->m_TargetCoupon = TargetCoupon;
+    d->m_SenioritySide = ssd;
+    d->m_SizeSide = szd;
+    d->m_CouponSide = scd;
+}
+
+DeferredInterestTrigger::DeferredInterestTrigger(const QString& lab)
+    : DeferredInterestTrigger()
+{
+    Q_D(DeferredInterestTrigger);
+    d->m_TriggerLabel = lab;
+}
+
+QDataStream& DeferredInterestTrigger::WriteToStream(QDataStream& stream) const {
+    Q_D(const DeferredInterestTrigger);
+	stream
+        << d->m_TargetSeniority
+        << d->m_TargetSeniorityLevel
+        << d->m_TargetCoupon
+        << static_cast<quint8>(d->m_SizeSide)
+        << static_cast<quint8>(d->m_SenioritySide)
+        << static_cast<quint8>(d->m_CouponSide)
+        << d->m_TargetSize
+		;
+	return stream;
+}
+QDataStream& DeferredInterestTrigger::LoadOldVersion(QDataStream& stream) {
+    Q_D(DeferredInterestTrigger);
+	quint8 TempChar;
+    d->m_TargetSeniority.SetLoadProtocolVersion(loadProtocolVersion()); stream >> d->m_TargetSeniority;
+    d->m_TargetSeniorityLevel.SetLoadProtocolVersion(loadProtocolVersion()); stream >> d->m_TargetSeniorityLevel;
+    d->m_TargetCoupon.SetLoadProtocolVersion(loadProtocolVersion()); stream >> d->m_TargetCoupon;
+    stream >> TempChar; d->m_SizeSide = static_cast<TriggerSizeSide>(TempChar);
+    stream >> TempChar; d->m_SenioritySide = static_cast<TriggerSenioritySide>(TempChar);
+    stream >> TempChar; d->m_CouponSide = static_cast<TriggerCouponSide>(TempChar);
+    d->m_TargetSize.SetLoadProtocolVersion(loadProtocolVersion()); stream >> d->m_TargetSize;
+	return AbstractTrigger::LoadOldVersion(stream);
+}
+
+bool DeferredInterestTrigger::Failing(const QList<Tranche*>& currentTranches, const QDate& CurrentPeriod) const
+{
+    return !Passing(currentTranches, CurrentPeriod);
+}
+
+bool DeferredInterestTrigger::Failing(const QList<Tranche*>& currentTranches, int CurrentPeriod) const
+{
+    return !Passing(currentTranches, CurrentPeriod);
+}
+
+QString DeferredInterestTrigger::ReadyToCalculate() const
+{
+    Q_D(const DeferredInterestTrigger);
+	if (
+        d->m_TargetSeniority.IsEmpty(1)
+        || d->m_SizeSide == TriggerSizeSide::Invalid
+        || d->m_SenioritySide == TriggerSenioritySide::Invalid
+        || d->m_CouponSide == TriggerCouponSide::Invalid
+        || d->m_TargetSeniorityLevel.IsEmpty(0)
+        || d->m_TargetCoupon.IsEmpty(0)
+        || d->m_TargetSize.IsEmpty(0.0)
+		)
+        return "Trigger " + d->m_TriggerLabel + " is invalid\n";
+	return QString();
+}
+
+const DeferredInterestTrigger::TriggerSizeSide& DeferredInterestTrigger::GetSizeSide() const
+{
+    Q_D(const DeferredInterestTrigger);
+    return d->m_SizeSide;
+}
+
+void DeferredInterestTrigger::SetSizeSide(const TriggerSizeSide& val)
+{
+    Q_D( DeferredInterestTrigger);
+    d->m_SizeSide = val;
+}
+
+const DeferredInterestTrigger::TriggerSenioritySide& DeferredInterestTrigger::GetSenioritySide() const
+{
+    Q_D(const DeferredInterestTrigger);
+    return d->m_SenioritySide;
+}
+
+void DeferredInterestTrigger::SetSenioritySide(const TriggerSenioritySide& val)
+{
+    Q_D( DeferredInterestTrigger);
+    d->m_SenioritySide = val;
+}
+
+const IntegerVector& DeferredInterestTrigger::GetTargetSeniorityLevel() const
+{
+    Q_D(const DeferredInterestTrigger);
+    return d->m_TargetSeniorityLevel;
+}
+
+void DeferredInterestTrigger::SetTargetSeniorityLevel(const QString& val)
+{
+    Q_D( DeferredInterestTrigger);
+    d->m_TargetSeniorityLevel = val;
+}
+
+const IntegerVector& DeferredInterestTrigger::GetTargetSeniority() const
+{
+    Q_D(const DeferredInterestTrigger);
+    return d->m_TargetSeniority;
+}
+
+void DeferredInterestTrigger::SetTargetSeniority(const QString& val)
+{
+    Q_D( DeferredInterestTrigger);
+    d->m_TargetSeniority = val;
+}
+
+const BloombergVector& DeferredInterestTrigger::GetTargetSize() const
+{
+    Q_D(const DeferredInterestTrigger);
+    return d->m_TargetSize;
+}
+
+void DeferredInterestTrigger::SetTargetSize(const QString& val)
+{
+    Q_D( DeferredInterestTrigger);
+    d->m_TargetSize = val;
+}
+
+const DeferredInterestTrigger::TriggerCouponSide& DeferredInterestTrigger::GetCouponSide() const
+{
+    Q_D(const DeferredInterestTrigger);
+    return d->m_CouponSide;
+}
+
+void DeferredInterestTrigger::SetCouponSide(const TriggerCouponSide& val)
+{
+    Q_D( DeferredInterestTrigger);
+    d->m_CouponSide = val;
+}
+
+const IntegerVector& DeferredInterestTrigger::GetTargetCoupon() const
+{
+    Q_D(const DeferredInterestTrigger);
+    return d->m_TargetCoupon;
+}
+
+void DeferredInterestTrigger::SetTargetCoupon(const QString& val)
+{
+    Q_D( DeferredInterestTrigger);
+    d->m_TargetCoupon = val;
+}
+
+bool DeferredInterestTrigger::Passing(const QList<Tranche*>& currentTranches, const QDate& CurrentPeriod) const
+{
+    Q_D(const DeferredInterestTrigger);
+    return d->passingTemplate(currentTranches, CurrentPeriod);
+}
+
+bool DeferredInterestTrigger::Passing(const QList<Tranche*>& currentTranches, int CurrentPeriod) const
+{
+    Q_D(const DeferredInterestTrigger);
+    return d->passingTemplate(currentTranches, CurrentPeriod);
+}
+
+
+QString DeferredInterestTrigger::ToString() const {
+    Q_D(const DeferredInterestTrigger);
+	return AbstractTrigger::ToString()
+        + "\nTarget Seniority: " + d->m_TargetSeniority.GetVector()
+        + "\nTarget Seniority Level: " + d->m_TargetSeniorityLevel.GetVector()
+        + "\nTarget Coupon: " + d->m_TargetCoupon.GetVector()
+        + "\nSize Limit: " + d->m_TargetSize.GetVector()
+        + "\nSize Side: " + TriggerSizeSideToString(d->m_SizeSide)
+        + "\nSeniority Side: " + TriggerSenioritySideToString(d->m_SenioritySide)
+        + "\nCoupon Side: " + TriggerCouponSideToString(d->m_CouponSide)
+	;
+}
+QString DeferredInterestTrigger::TriggerCouponSideToString(TriggerCouponSide a) const {
+	switch (a) {
+	case TriggerCouponSide::Invalid: return "Invalid";
+	case TriggerCouponSide::Bigger: return "Bigger";
+	case TriggerCouponSide::Smaller: return "Smaller";
+	case TriggerCouponSide::Exactly: return "Exactly";
+	case TriggerCouponSide::BiggerOrEqual: return "Bigger or Equal";
+	case TriggerCouponSide::SmallerOrEqual: return "Smaller or Equal";
+	default: return QString();
+	}
+}
+QString DeferredInterestTrigger::TriggerSizeSideToString(TriggerSizeSide a) const {
+	switch (a) {
+	case TriggerSizeSide::Invalid: return "Invalid";
+	case TriggerSizeSide::Bigger: return "Bigger";
+	case TriggerSizeSide::Smaller: return "Smaller";
+	case TriggerSizeSide::Exactly: return "Exactly";
+	case TriggerSizeSide::BiggerOrEqual: return "Bigger or Equal";
+	case TriggerSizeSide::SmallerOrEqual: return "Smaller or Equal";
+	default: return QString();
+	}
+}
+QString DeferredInterestTrigger::TriggerSenioritySideToString(TriggerSenioritySide a) const {
+	switch (a) {
+	case TriggerSenioritySide::Invalid: return "Invalid";
+	case TriggerSenioritySide::Senior: return "Senior";
+	case TriggerSenioritySide::Junior: return "Junior";
+	case TriggerSenioritySide::Exactly: return "Exactly";
+	case TriggerSenioritySide::SeniorOrEqual: return "Senior or Equal";
+	case TriggerSenioritySide::JuniorOrEqual: return "Junior or Equal";
+	default: return QString();
+	}
+}
+
+bool DeferredInterestTrigger::HasAnchor() const {
+    Q_D(const DeferredInterestTrigger);
+	return !(
+        d->m_TargetSize.GetAnchorDate().isNull()
+        || d->m_TargetSeniorityLevel.GetAnchorDate().isNull()
+        || d->m_TargetSeniority.GetAnchorDate().isNull()
+        || d->m_TargetCoupon.GetAnchorDate().isNull()
+		);
+}
+
+void DeferredInterestTrigger::FillMissingAnchorDate(const QDate& a) {
+    Q_D( DeferredInterestTrigger);
+    if (d->m_TargetSize.GetAnchorDate().isNull()) 
+        d->m_TargetSize.SetAnchorDate(a);
+    if (d->m_TargetSeniorityLevel.GetAnchorDate().isNull()) 
+        d->m_TargetSeniorityLevel.SetAnchorDate(a);
+    if (d->m_TargetSeniority.GetAnchorDate().isNull()) 
+        d->m_TargetSeniority.SetAnchorDate(a);
+    if (d->m_TargetCoupon.GetAnchorDate().isNull()) 
+        d->m_TargetCoupon.SetAnchorDate(a);
+}
+
+void DeferredInterestTrigger::SetAnchorDate(const QDate& a)
+{
+    Q_D( DeferredInterestTrigger);
+    d->m_TargetSize.SetAnchorDate(a);
+    d->m_TargetSeniorityLevel.SetAnchorDate(a); 
+    d->m_TargetSeniority.SetAnchorDate(a);
+}
+
+void DeferredInterestTrigger::RemoveAnchorDate()
+{
+    Q_D( DeferredInterestTrigger);
+    d->m_TargetSize.RemoveAnchorDate(); 
+    d->m_TargetSeniorityLevel.RemoveAnchorDate();
+    d->m_TargetSeniority.RemoveAnchorDate();
+}
+
+void DeferredInterestTrigger::SetSizeMultiplier(double mult)
+{
+    Q_D( DeferredInterestTrigger);
+	 ResetSizeMultiplier();
+	 if (mult != 0.0)
+         d->m_TargetSize *= mult;
+}
+
+void DeferredInterestTrigger::ResetSizeMultiplier()
+{
+    Q_D( DeferredInterestTrigger);
+    d->m_TargetSize.SetDivisor(1.0);
+}
---
+++ CLOModelLib/DeferredInterestTrigger.h
@@ -0,0 +1,81 @@
+#ifndef DeferredInterestTrigger_h__
+#define DeferredInterestTrigger_h__
+#include "AbstractTrigger.h"
+class Tranche;
+class IntegerVector;
+class DeferredInterestTriggerPrivate;
+class DeferredInterestTrigger : public AbstractTrigger {
+    DECLARE_PUBLIC_COMMONS(DeferredInterestTrigger)
+    DECLARE_PUBLIC_COMMONS_COPY(DeferredInterestTrigger)
+public:
+	enum class TriggerSizeSide : quint8 {
+		Invalid = 0
+		, Bigger = 0x1
+		, Smaller = 0x2
+		, Exactly = 0x4
+		, BiggerOrEqual = Bigger | Exactly
+		, SmallerOrEqual = Smaller | Exactly
+	};
+	enum class TriggerSenioritySide : quint8 {
+		Invalid = 0
+		, Senior = 0x1
+		, Junior = 0x2
+		, Exactly = 0x4
+		, SeniorOrEqual = Senior | Exactly
+		, JuniorOrEqual = Junior | Exactly
+	};
+	enum class TriggerCouponSide : quint8 {
+		Invalid = 0
+		, Bigger = 0x1
+		, Smaller = 0x2
+		, Exactly = 0x4
+		, BiggerOrEqual = Bigger | Exactly
+		, SmallerOrEqual = Smaller | Exactly
+	};
+
+	QString TriggerSizeSideToString(TriggerSizeSide a) const;
+	QString TriggerSenioritySideToString(TriggerSenioritySide a) const;
+	QString TriggerCouponSideToString(TriggerCouponSide a) const;
+	DeferredInterestTrigger(
+		QString TargetSenior
+		, QString TargetSeniorLevel
+		, QString TargetSize = "0"
+		, QString TargetCoupon = "0"
+		, TriggerSizeSide szd = TriggerSizeSide::Invalid
+		, TriggerSenioritySide ssd = TriggerSenioritySide::Invalid
+		, TriggerCouponSide scd = TriggerCouponSide::Invalid
+		, const QString& lab = QString()
+	);
+	DeferredInterestTrigger(const QString& lab);
+	virtual bool Passing(const QList<Tranche*>& currentTranches, const QDate& CurrentPeriod) const;
+	virtual bool Passing(const QList<Tranche*>& currentTranches, int CurrentPeriod) const;
+	virtual bool Failing(const QList<Tranche*>& currentTranches, const QDate& CurrentPeriod) const;
+	virtual bool Failing(const QList<Tranche*>& currentTranches, int CurrentPeriod) const;
+	virtual QString ReadyToCalculate() const override;
+	const DeferredInterestTrigger::TriggerSizeSide& GetSizeSide() const;
+	void SetSizeSide(const DeferredInterestTrigger::TriggerSizeSide& val);
+	const DeferredInterestTrigger::TriggerSenioritySide& GetSenioritySide() const;
+	void SetSenioritySide(const DeferredInterestTrigger::TriggerSenioritySide& val);
+	const IntegerVector& GetTargetSeniorityLevel() const;
+	void SetTargetSeniorityLevel(const QString& val);
+	const IntegerVector& GetTargetSeniority() const;
+	void SetTargetSeniority(const QString& val);
+	const BloombergVector& GetTargetSize() const;
+	void SetTargetSize(const QString& val);
+	const DeferredInterestTrigger::TriggerCouponSide& GetCouponSide() const;
+	void SetCouponSide(const DeferredInterestTrigger::TriggerCouponSide& val);
+	const IntegerVector& GetTargetCoupon() const;
+	void SetTargetCoupon(const QString& val);
+	void SetSizeMultiplier(double mult);
+	void ResetSizeMultiplier();
+	virtual QString ToString() const override;
+	bool HasAnchor()const;
+	void FillMissingAnchorDate(const QDate& a);
+	void SetAnchorDate(const QDate& a);
+	void RemoveAnchorDate();
+protected:
+    virtual QDataStream& WriteToStream(QDataStream& stream) const override;
+    virtual QDataStream& LoadOldVersion(QDataStream& stream) override;
+};
+
+#endif // DeferredInterestTrigger_h__

5e89c862b5b7f657f9a066396219a8dc60f1e1b2
Luca Beldi <luca.beldi@twentyfouram.com>
2015-09-24 10:04:21 +0100

    Moved WatFalPrior to garbage collection

--- CLOModelLib/Private/WatFalPrior_p.h
+++ CLOModelLib/Private/WatFalPrior_p.h
@@ -4,14 +4,15 @@
 #include "WatFalPrior.h"
 #include "Private/BackwardCompatibilityInterface_p.h"
 #include <QSet>
+#include <memory>
 class WatFalPriorPrivate : public BackwardInterfacePrivate
 {
     DECLARE_PRIVATE_COMMONS(WatFalPrior)
     DECLARE_PRIVATE_COMMONS_COPY(WatFalPrior)
     DECLARE_PRIVATE_COMMONS_DATASTREAM(WatFalPrior)
 public:
-    QHash<qint32, IntegerVector*> IntParameters;
-    QHash<qint32, BloombergVector*> DoubleParameters;
+    QHash<qint32, std::shared_ptr<IntegerVector> > IntParameters;
+    QHash<qint32, std::shared_ptr<BloombergVector> > DoubleParameters;
     QString TriggerStruc;
     WatFalPrior::wstAccrueOrPay m_AccrueOrPay;
     WatFalPrior::WaterfallStepType PriorityType;

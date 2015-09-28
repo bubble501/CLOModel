6d8c3143aebf1389640c7cbc3d6629d06ee9a035
Luca Beldi <luca.beldi@twentyfouram.com>
2015-09-28 09:48:43 +0100

    WaterfallCalculator now uses files instead of loading everything in memory
    Relates to #252

--- CLOModelLib/WaterfallCalculator.h
+++ CLOModelLib/WaterfallCalculator.h
@@ -21,6 +21,9 @@ public:
 	virtual int NumBees() const override;
 	virtual void Reset() override;
 	virtual void ClearWaterfalls();
+    virtual void RemoveResult(qint32 Key) override;
+    virtual const std::shared_ptr<Waterfall> GetResult(qint32 key)const override;
+    virtual void ClearResults() override;
 public slots:
 	virtual bool StartCalculation() override;
     friend CLOMODELLIB_EXPORT QDataStream& operator<<(QDataStream & stream, const WaterfallCalculator& flows);

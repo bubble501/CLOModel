74f351a3d82f2ac1593f5ca159fc226077a61b07
Luca Beldi <luca.beldi@twentyfouram.com>
2015-10-27 11:31:24 +0000

    Use std::array to avoid compiler warning and use checked iterators

--- CLOModelLib/Private/Ratings_p.h
+++ CLOModelLib/Private/Ratings_p.h
@@ -1,6 +1,6 @@
 #ifndef Ratings_p_h__
 #define Ratings_p_h__
-
+#include <array>
 #include "Ratings.h"
 #include "Private/BackwardCompatibilityInterface_p.h"
 class RatingsPrivate : public BackwardInterfacePrivate
@@ -9,8 +9,8 @@ class RatingsPrivate : public BackwardInterfacePrivate
     DECLARE_PRIVATE_COMMONS_COPY(Ratings)
     DECLARE_PRIVATE_COMMONS_DATASTREAM(Ratings)
 public:
-    Ratings::RatingValue m_ratings[Ratings::CountRatingAcencies];
-    Ratings::CreditWatch m_watch[Ratings::CountRatingAcencies];
+    std::array<Ratings::RatingValue, Ratings::CountRatingAcencies> m_ratings;
+    std::array<Ratings::CreditWatch, Ratings::CountRatingAcencies> m_watch;
     static const QString m_ratingSyntax[Ratings::CountRatingAcencies][static_cast<qint16>(Ratings::RatingValue::Dm) + 1];
     static const QString m_ratingFields[Ratings::CountRatingAcencies];
     static const QString m_AgencyName[Ratings::CountRatingAcencies];
